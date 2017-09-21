66202df9f1ba6ad6dce269313db55e465588ea45
Camille Gicquel <camille.gicquel@corp.ovh.com>
2017-09-21 15:19:04 +0200

    :sparkles: feat(docs): add gitignore and exclude

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,11 +2,8 @@ var navbarButton = document.getElementById('navbar-toggle');
 var navMain = document.getElementById('navbar-entries');
 var navbarToggle = false;
 
-console.log(navMain);
-
 var toggleNav = function() {
     navbarToggle = !navbarToggle;
-    console.log(navbarToggle);
 
     if (navbarToggle) {
         navMain.classList.add('navbar__entries--active');
