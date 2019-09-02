d21e914a0e2fd09a7df04b5a546f668db26305d8
ZENNED Abderrazak <abderrazak.zenned.ext@corp.ovh.com>
2019-09-02 11:18:14 +0200

    fix: adding sshfs port image

--- lib/gen-config.sh
+++ lib/gen-config.sh
@@ -232,20 +232,17 @@ fi
 
 # Additional SSHFS variables
 PUBLIC_SSH_PORT=""
-if [ "${PUBLIC_SSH_PORT}" == 1 ]; then
-  ## PUBLIC_SSH_PORT
-  while true; do
-    printf "\e[1mEnter the SSH  remote port:\e[0m "
-    read PUBLIC_SSH_PORT
+while true; do
+  printf "\e[1mEnter the SSH  remote port:\e[0m "
+  read PUBLIC_SSH_PORT
 
-    REGEX='^[0-9]+$';
-    if ! [[ $PUBLIC_SSH_PORT =~ $REGEX ]]; then
-        echo "Invalid input. Please use only numbers."
-    else
-        break;
-    fi
-  done
-fi
+  REGEX='^[0-9]+$';
+  if ! [[ $PUBLIC_SSH_PORT =~ $REGEX ]]; then
+      echo "Invalid input. Please use only numbers."
+  else
+      break;
+  fi
+done
 
 # ---
 
